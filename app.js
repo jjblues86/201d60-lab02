f896a4b0ebda03b145b243ce72e32e1cec2af06d
Jerome Joof <jeromejoof@yahoo.com>
2019-11-06 13:36:26 -0800

    working on a guessing game about Jerome

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// var kids = prompt('How many kids does jerome have?');
+// console.log('number of kids:', kids);
+
+// var shoes = prompt('How many shoes does Jerome have?');
+// console.log('num shoes:', shoes);
+
+// if(kids === '2' || kids === '3'){
+//     console.log('kids eval:', 'wonderful');
+// }
+
+// if(shoes === '6' || kids === '6'){
+//     console.log('shoes eval:', 'right')
+// }
+
+var userName = prompt('Welcome to my site! What is your name?');
+console.log('Welcome to my site ' + userName + '!');
+
+//Origin
+var origin = prompt('Where is Jerome from?');
+origin = origin.toUpperCase();
+console.log('Where is Jerome from? ' + origin);
+
+//Kids
+var kids = prompt('Does Jerome have any kids?');
+kids = kids.toUpperCase();
+console.log('Does Jerome have any kids? ' + kids);
