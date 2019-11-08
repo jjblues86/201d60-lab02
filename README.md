408d111d40444418b111a236c93eb19aa4541755
Jerome Joof <jeromejoof@yahoo.com>
2019-11-07 16:01:18 -0800

    added more questions to my guessing game

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 201d60-lab02
+material 
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use strict';
+var correctGuess = 0;
 
 var userName = prompt('Welcome to my site! What is your name?');
 console.log('Welcome to my site ' + userName + '!');
@@ -11,6 +12,7 @@ console.log('Where is Jerome from? ' + origin);
 
 if(origin === 'THE GAMBIA' || origin === 'the gambia'){
     alert('You are right!');
+    correctGuess++;
 } else {
     alert('Jerome is from The Gambia')
 }
@@ -22,6 +24,7 @@ console.log('Does Jerome have any kids? ' + kids);
 
 if(kids === 'YES' || kids === 'y'){
     alert('You got it');
+    correctGuess++;
 } else {
     alert('I have one kid');
 }
@@ -33,6 +36,7 @@ console.log('Is soccer Jerome\'s favorite sport? ' + sports);
 
 if(sports === 'YES' || sports === 'y'){
     alert('Hell Yeah!');
+    correctGuess++;
 } else {
     alert('Actually soccer is my favorite sport');
 }
@@ -44,6 +48,7 @@ console.log('How many Countries has Jerome visited? ' + visited);
 
 if(visited === 'NO' || visited === 'no'){
     alert('You guessed it right');
+    correctGuess++;
 } else {
     alert('I have never been to China before');
 }
@@ -55,6 +60,55 @@ console.log('Does Jerome speak spanish? ' + language);
 
 if(language === 'NO' || language === 'no'){
     alert('Mi no habla espanola');
+    correctGuess++;
 } else {
     alert('You really believe in me :)');
 }
+
+// Let user guess your favorite number
+var myNum = 8;
+var tries = 4;
+
+while(tries > 0){
+
+    var userInput = prompt('Can you guess my favorite number?');
+    var userInputInt = parseInt(userInput);
+
+    if(userInputInt < myNum){
+        alert('too small');
+        tries --;
+    } else if(userInputInt > myNum){
+        alert('too large');
+        tries --;
+    } else if(userInputInt === NaN || userInputInt === ''){
+        alert('You need to enter a value');
+    }
+    else if(userInputInt === myNum){
+        tries = 0;
+        alert('You are a winner 👯‍♂️');
+        correctGuess++;
+        break;
+        correctGuess--;
+    }
+}
+
+// Guess my favorite colors
+var colors = ['blue', 'red', 'grey', 'purple'];
+// var len = colors.length;
+
+for( var i = 5; i >= 0; i--){
+
+    var userGuess = prompt('Can you guess my favorite colors?');
+    userGuess = userGuess.toLowerCase();
+
+    if(colors.includes(userGuess)){
+        alert('That was a good guess buddy. Blue, Red, Grey and Purple are my favorite colors');
+        console.log(userGuess);
+        correctGuess++;
+        break;
+    } else {
+        alert('Please continue guessing. You have ' + i + ' guesses left');
+    }
+}
+
+alert('You guessed ' + correctGuess + ' out of 7 correct');
